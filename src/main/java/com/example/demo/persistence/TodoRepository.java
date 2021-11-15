e36822b5e88a02542bd6ac0dde710ef6835b269d
YoungDogg <leejonghwo@gmail.com>
2021-11-15 15:26:47 +0900

    com.example.demo.persistence @Query 오류 해결
    해결법을 주석으로 달았다

--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -13,5 +13,6 @@ public interface TodoRepository extends JpaRepository<TodoEntity, String>{
 	 
 	// ?1은 매서드의 매개변수의 순서위치다.
 	//@Query("select * from Todo t where t.userId = ?1")
+	// 오류 해결법 : Todo의 풀네임을 적어주면 된다고 한다. 예) com.example.demo.persistence.Todo...
 	List<TodoEntity> findByUserId(String userId);
 }
