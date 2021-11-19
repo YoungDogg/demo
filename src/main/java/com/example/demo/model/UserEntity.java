c1d96c8e776d2bf8d2c5e0df4a2e062ea08bd5a2
YoungDogg <leejonghwo@gmail.com>
2021-11-19 22:41:56 +0900

    ~4.2 레이어 구현
    4.1  REST API 인증 기법
    4.2 User 레이어 구현

---
+++ src/main/java/com/example/demo/model/UserEntity.java
@@ -0,0 +1,37 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table( uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class UserEntity {
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name="system-uuid", strategy = "uuid")
+	private String id; // 고유 id
+	
+	@Column(nullable = false)
+	private String username;
+	
+	@Column(nullable = false)
+	private String email; // id와 같은 기능
+	
+	@Column(nullable = false)
+	private String password; // 패스워드 
+}
