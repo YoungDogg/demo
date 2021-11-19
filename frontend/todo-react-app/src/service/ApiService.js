9ab0d8d1017ca7de4f60a3c500836bde9c8daa21
YoungDogg <leejonghwo@gmail.com>
2021-11-19 15:31:43 +0900

    ~3.3.4 정리

--- frontend/todo-react-app/src/service/ApiService.js
+++ frontend/todo-react-app/src/service/ApiService.js
@@ -1,24 +1,24 @@
-import { API_BASE_URL } from "../api-config";
+import { API_BASE_URL } from "../app-config";
 
-export function call(api, method, request){
-    options = {
-        headers : new Headers({
-            "Content-Type" : "application/json",
-        }),
-        url: API_BASE_URL + api,
-        method: method
-    };
-
-    if(request){
-        //GET method
-        options.body = JSON.stringify(request);
-    }
-    return fetch(options.url, options).then((response) =>
-     response.json().then((json) => {
-         if(!response.ok){
-             //response.ok가 true이면 정상적인 응답을 받은 것이고 아니면 에러
-             return Promise.reject(json);
-         }
-         return json;
-     }))
+export function call(api, method, request) {
+  let options = {
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+    url: API_BASE_URL + api,
+    method: method,
+  };
+  if (request) {
+    // GET method
+    options.body = JSON.stringify(request);
+  }
+  return fetch(options.url, options).then((response) =>
+    response.json().then((json) => {
+      if (!response.ok) {
+        // response.ok가 true이면 정상적인 리스폰스를 받은것, 아니면 에러 리스폰스를 받은것.
+        return Promise.reject(json);
+      }
+      return json;
+    })
+  );
 }
